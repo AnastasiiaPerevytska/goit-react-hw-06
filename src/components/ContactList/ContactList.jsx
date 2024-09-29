ad5ddd2e3ab778a5af9b117dd096e20c7af2ca7d
AnastasiiaPerevytska <anastasiia.zelenina.1995@gmail.com>
2024-09-29 15:54:02 +0300

    hw6 done

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+import Contact from "../Contact/Contact";
+import styles from "./ContactList.module.css";
+
+export default function ContactList() {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+// console.log(contacts);
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul className={styles.list}>
+      {filteredContacts.map((contact) => (
+        <Contact key={contact.id} contact={contact} />
+      ))}
+    </ul>
+  );
+}
